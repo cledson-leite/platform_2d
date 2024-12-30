fb087e58a4a299399cfcafb001b12fe9ce79f155
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2024-12-30 18:12:44 -0300

    paralax

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_FORCE = -400.0
 
 @onready var animation: AnimatedSprite2D = $animy
 
@@ -16,8 +16,10 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_FORCE
 		is_jump = true
+	elif is_on_floor():
+		is_jump = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
